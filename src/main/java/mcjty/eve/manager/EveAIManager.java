1d96cb917206138cf9e663ef8535f669238fb593
McJty <mcjty1@gmail.com>
2017-01-13 16:21:08 +0100

    Stuff

--- src/main/java/mcjty/eve/manager/EveAIManager.java
+++ src/main/java/mcjty/eve/manager/EveAIManager.java
@@ -9,7 +9,45 @@ public class EveAIManager {
 
         data.nextTick();
 
+        switch (data.getStrength()) {
+            case SLEEP:
+                break;
+            case WEAK:
+                handleWeakState(world, data);
+                break;
+            case NORMAL:
+                handleNormalState(world, data);
+                break;
+            case STRONG:
+                handleStrongState(world, data);
+                break;
+            case DEADLY:
+                handleDeadlyState(world, data);
+                break;
+        }
+
         EveWorldSavedData.save(world);
+    }
+
+    private static void handleWeakState(World world, EveData data) {
+
+    }
+
+    private static void handleNormalState(World world, EveData data) {
+        resolveTarget(world, data);
+    }
+
+    private static void handleStrongState(World world, EveData data) {
+        resolveTarget(world, data);
+    }
+
+    private static void handleDeadlyState(World world, EveData data) {
+        resolveTarget(world, data);
+    }
+
+    // If Eve has no target or the target is not online try to find another target
+    private static void resolveTarget(World world, EveData data) {
+
     }
 
 }
